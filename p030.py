b4b3235829dc1c8954491d96a3328167b542ee64
hjtran <j.tran4418@gmail.com>
2016-05-22 20:56:12 -0700

    more solns

--- p030.py
+++ p030.py
@@ -1,28 +1,7 @@
-'''#unsolved
-numWaysToMake = [0]*201
-#coinSizes = [1,2,5,10,20,50,100,200]
-coinSizes = [1,5,10,25]
 
-for c in coinSizes:
-    numWaysToMake[c] = 1
-
-for i in range(len(numWaysToMake)):
-    for c in coinSizes:
-        if i-c>0:
-            numWaysToMake[i] += numWaysToMake[i-c]
-        else:
-            break
-
-    
-
-print numWaysToMake[100]'''
-
-coinSizes = [200,100,50,20,10,5,2,1]
-numCoins = [0]*len(coinSizes)
-count = 0
-coinSum = 200
-
-ptr = 0
-while numCoins[-1]!=sum:
-    while sum([s*c for s,c in zip(coinSizes,numCoins)]) <= 200:
-        if 
+s = []
+for i in range(2,2000000):
+    digs = [ int(c) for c in str(i) ]
+    if i == sum( [ dig ** 5 for dig in digs ] ):
+        s.append(i)
+print sum(s)
